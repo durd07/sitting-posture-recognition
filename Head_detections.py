2c4c1c1cd04c10a42ad7c0065ed030aba7a35b39
Felix Du <felix.du@nokia-sbell.com>
2024-06-08 19:36:11 +0800

    update

--- Head_detections.py
+++ Head_detections.py
@@ -1,7 +1,7 @@
-def Head_detection(Head_detection_data):#p0，p2，p5
-    if Head_detection_data[1]>Head_detection_data[2]+6:
+def Head_detection(Head_detection_data):  # p0，p2，p5
+    if Head_detection_data[1] > Head_detection_data[2] + 6:
         return 1
-    if Head_detection_data[1]+6<Head_detection_data[2]:
+    if Head_detection_data[1] + 6 < Head_detection_data[2]:
         return 2
-    if abs(Head_detection_data[1]-Head_detection_data[2])<6:
+    if abs(Head_detection_data[1] - Head_detection_data[2]) < 6:
         return 3
--- yonghu_ui_main.py
+++ yonghu_ui_main.py
@@ -1,6 +1,6 @@
 import os
 import sys
-#import winsound
+# import winsound
 import cv2
 import mediapipe as mp
 import time
@@ -23,49 +23,55 @@ import emails as em
 # cuowu_time=0
 
 yonghu_name = "123"
-class poseDetector():
-    def __init__(self, mode= False, complex=1, smooth=True, enable=False, segment=True,
-                 detectionCon=0.5, trackCon=0.5):
-        self.mode=mode
-        self.complex=complex
-        self.smooth=smooth
-        self.enable=enable
-        self.segment=segment
-        self.detectionCon=detectionCon
-        self.trackCon=trackCon
-        self.mpDraw=mp.solutions.drawing_utils
-        self.mpPose=mp.solutions.pose #人体姿态检测
-        self.pose=self.mpPose.Pose(self.mode,self.complex,self.smooth,self.enable,self.segment,
-                                   self.detectionCon,self.trackCon)#姿态关键点检测函数
 
-    def findpose(self,img,draw=True):
+
+class PoseDetector():
+    def __init__(self, mode=False, complex=1, smooth=True, enable=False, segment=True,
+                 detectionCon=0.5, trackCon=0.5):
+        self.mode = mode
+        self.complex = complex
+        self.smooth = smooth
+        self.enable = enable
+        self.segment = segment
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+        self.mpDraw = mp.solutions.drawing_utils
+        self.mpPose = mp.solutions.pose  # 人体姿态检测
+        self.pose = self.mpPose.Pose(self.mode, self.complex, self.smooth, self.enable, self.segment,
+                                     self.detectionCon, self.trackCon)  # 姿态关键点检测函数
+
+    def findpose(self, img, draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # 将导入的BGR格式图像转为RGB格式
         self.results = self.pose.process(imgRGB)  # 将图像传给姿态识别模型
-        if self.results.pose_landmarks: # 如果采集到图像
+        if self.results.pose_landmarks:  # 如果采集到图像
+            print("pose_landmarks", self.results.pose_landmarks)
             if draw:
                 self.mpDraw.draw_landmarks(img, self.results.pose_landmarks,
                                            self.mpPose.POSE_CONNECTIONS)  # 绘制姿态坐标点，img为画板，传入姿态点坐标，坐标连线
         return img
 
-    def findPosition(self,img,draw=True):
-        lmList=[]
+    def findPosition(self, img, draw=True):
+        lmList = []
         if self.results.pose_landmarks:
-            for id,lm in enumerate(self.results.pose_landmarks.landmark):
-                h,w,c=img.shape
-                cx,cy=int(lm.x * w),int(lm.y * h)
-                lmList.append([id,cx,cy])
+            for id, lm in enumerate(self.results.pose_landmarks.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                lmList.append([id, cx, cy])
                 if draw:
-                    cv2.circle(img,(cx,cy),5,(255,0,0),cv2.FILLED)
+                    cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
         return lmList
+
 
 def jinggao():
     print("Please Sit Correctly")
-    #pt =pyttsx3.init()
-    #pt.say("请调整坐姿")
-    #pt.runAndWait()
+    # pt =pyttsx3.init()
+    # pt.say("请调整坐姿")
+    # pt.runAndWait()
+
 
 class Voice_Thread(QThread):
     sinout = pyqtSignal(int)
+
     def __init__(self):
         super(Voice_Thread, self).__init__()
 
@@ -77,8 +83,10 @@ class Voice_Thread(QThread):
         # 发出信号
         self.sinout.emit(1)
 
+
 class Email_Thread(QThread):
-    sinout1=pyqtSignal(int)
+    sinout1 = pyqtSignal(int)
+
     def __init__(self):
         super(Email_Thread, self).__init__()
 
@@ -87,9 +95,10 @@ class Email_Thread(QThread):
         # 需要执行的内容
         # jinggao()
         # print(yonghu_email)
-        #print(em.mail(yonghu_email))
+        # print(em.mail(yonghu_email))
         # 发出信号
         self.sinout1.emit(1)
+
 
 class Ui_MainWindow(QtWidgets.QWidget):
 
@@ -104,14 +113,14 @@ class Ui_MainWindow(QtWidgets.QWidget):
         self.__flag_work = 0
         self.x = 0
         self.count = 0
-        self.voice_cuowu_time=0
-        self.email_cuowu_time=0
-        self.pose1=poseDetector()
-        self.lmList=[]
+        self.voice_cuowu_time = 0
+        self.email_cuowu_time = 0
+        self.pose1 = PoseDetector()
+        self.lmList = []
         self.pTime = 0
         self.timeThread = Voice_Thread()
         self.timeThread.sinout.connect(self.show_Stop)
-        self.timeThread1=Email_Thread()
+        self.timeThread1 = Email_Thread()
         self.timeThread1.sinout1.connect(self.show_Stop1)
 
     def show_Stop(self):
@@ -122,7 +131,7 @@ class Ui_MainWindow(QtWidgets.QWidget):
 
     def set_ui(self):
         self.__layout_main = QtWidgets.QHBoxLayout()  # 采用QHBoxLayout类，按照从左到右的顺序来添加控件
-        self.__layout_xinxi=QtWidgets.QVBoxLayout()
+        self.__layout_xinxi = QtWidgets.QVBoxLayout()
         self.__layout_fun_button = QtWidgets.QHBoxLayout()
         self.__layout_data_show = QtWidgets.QVBoxLayout()  # QVBoxLayout类垂直地摆放小部件
 
@@ -133,11 +142,11 @@ class Ui_MainWindow(QtWidgets.QWidget):
         button_color = [self.button_open_camera, self.button_close]
         for i in range(2):
             button_color[i].setStyleSheet("QPushButton{color:black}"
-                                           "QPushButton:hover{color:red}"
-                                           "QPushButton{background-color:rgb(78,255,255)}"
-                                           "QpushButton{border:2px}"
-                                           "QPushButton{border_radius:10px}"
-                                           "QPushButton{padding:2px 4px}")
+                                          "QPushButton:hover{color:red}"
+                                          "QPushButton{background-color:rgb(78,255,255)}"
+                                          "QpushButton{border:2px}"
+                                          "QPushButton{border_radius:10px}"
+                                          "QPushButton{padding:2px 4px}")
 
         self.button_open_camera.setMinimumHeight(50)
         self.button_close.setMinimumHeight(50)
@@ -220,20 +229,20 @@ class Ui_MainWindow(QtWidgets.QWidget):
         fps = 1 / (cTime - self.pTime)
         self.pTime = cTime
 
-        cv2.putText(self.image, str(int(fps)),     (30,50),   cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
-        cv2.putText(self.image, "Head tilt left",  (30, 125), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
+        cv2.putText(self.image, str(int(fps)), (30, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+        cv2.putText(self.image, "Head tilt left", (30, 125), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
         cv2.putText(self.image, "Head tilt right", (30, 150), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
-        cv2.putText(self.image, "Body tilt left",  (30, 175), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
+        cv2.putText(self.image, "Body tilt left", (30, 175), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
         cv2.putText(self.image, "Body tilt right", (30, 200), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
-        cv2.putText(self.image, "Squint",          (30, 225), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
-        cv2.putText(self.image, "Head forwards",   (30, 250), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
-        cv2.putText(self.image, "Sit correctly",   (30, 100), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
+        cv2.putText(self.image, "Squint", (30, 225), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
+        cv2.putText(self.image, "Head forwards", (30, 250), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
+        cv2.putText(self.image, "Sit correctly", (30, 100), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
 
-        self.image=self.pose1.findpose(self.image)
-        self.lmList=self.pose1.findPosition(self.image)
+        self.image = self.pose1.findpose(self.image)
+        self.lmList = self.pose1.findPosition(self.image)
 
-        # print(self.lmList)
-        if len(self.lmList)>0:
+        print(self.lmList)
+        if len(self.lmList) > 0:
             Head_detection_data.append(self.lmList[0][2])
             Physical_detection_data.append(self.lmList[0][2])
             Squint_detection_data.append(self.lmList[0][1])
@@ -274,21 +283,21 @@ class Ui_MainWindow(QtWidgets.QWidget):
             if head_left == 0 or head_right == 0 or body_left == 0 or body_right == 0 or squint == 0 or head_forward == 0:
                 self.voice_cuowu_time = self.voice_cuowu_time + 1
                 self.email_cuowu_time = self.email_cuowu_time + 1
-            if(self.voice_cuowu_time == 30):
+            if (self.voice_cuowu_time == 30):
                 self.timeThread.start()
                 self.voice_cuowu_time = 0
-            if(self.email_cuowu_time == 300):
+            if (self.email_cuowu_time == 300):
                 self.timeThread1.start()
                 self.email_cuowu_time = 0
 
-                    # self.zhuyemian_signal.emit()
+                # self.zhuyemian_signal.emit()
             # if head_left==0 or head_right==0 or body_left==0 or body_right==0 or squint==0 or head_forward==0 :
             #     duration = 1000  # millisecond
             #     freq = 1440  # Hz
             #     winsound.Beep(freq, duration)
             if true_sit == 1:
-                self.voice_cuowu_time=0
-                self.email_cuowu_time=0
+                self.voice_cuowu_time = 0
+                self.email_cuowu_time = 0
                 cv2.putText(self.image, "Sit correctly", (30, 100), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
             show = cv2.resize(self.image, (640, 480))
             show = cv2.cvtColor(show, cv2.COLOR_BGR2RGB)
@@ -316,25 +325,24 @@ class Ui_MainWindow(QtWidgets.QWidget):
 
 
 if __name__ == '__main__':
-    #with open('D:\pythonProject5\PyQt5_yemian\结果存放', "r", encoding='utf-8') as file_obj:
+    # with open('D:\pythonProject5\PyQt5_yemian\结果存放', "r", encoding='utf-8') as file_obj:
     #    contents = file_obj.read()
     #    x = contents.split("-")
     #    yonghu_name = x[0]
     #    yonghu_password = x[1]  # 未完善
     #    print(yonghu_name)
-    ## conn = pymysql.connect(host='localhost', port=3306, user='root', password='nuliba520.', db='test')
-    #conn=sqlite3.connect('yonghu1.db')
-    #cursor = conn.cursor()
-    #cursor.execute("SELECT * FROM users WHERE username=? ", (yonghu_name,))
-    #result = cursor.fetchall()
-    #for row in result:
+    # conn = pymysql.connect(host='localhost', port=3306, user='root', password='nuliba520.', db='test')
+    # conn=sqlite3.connect('yonghu1.db')
+    # cursor = conn.cursor()
+    # cursor.execute("SELECT * FROM users WHERE username=? ", (yonghu_name,))
+    # result = cursor.fetchall()
+    # for row in result:
     #    yonghu_name = row[0]
     #    yonghu_password = row[1]
     #    yonghu_number = row[2]
     #    yonghu_email = row[3]
-    #os.remove('D:\pythonProject5\PyQt5_yemian\结果存放')
+    # os.remove('D:\pythonProject5\PyQt5_yemian\结果存放')
     App = QApplication(sys.argv)
     win = Ui_MainWindow()
     win.show()
     sys.exit(App.exec_())
-
